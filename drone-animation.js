30e200c22a6a1b1529afaefd70192848b45ce640
Luciano Torrano <lucianotorrano.dev@gmail.com>
2022-07-22 01:56:18 -0300

    drone animation improved

--- drone-animation.js
+++ drone-animation.js
@@ -1,7 +1,18 @@
 const drone = document.getElementById('drone');
 const droneBox = document.getElementById('drone-service');
+let animationFinished = false;
 
-droneBox.addEventListener('click',()=>{
-    drone.classList.add('drone-animation');
-    droneBox.style.cursor = 'default';
-})
+droneBox.addEventListener('mouseover',()=>{
+    if(!animationFinished){
+        droneBox.style.transform = 'scale(1.1)'
+        droneBox.addEventListener('click',()=>{
+            drone.classList.add('drone-animation');
+            droneBox.style.cursor = 'default';
+            droneBox.style.transform = 'scale(1)'
+            animationFinished = true;
+        }
+    )}
+});
+droneBox.addEventListener('mouseout',()=>{
+    droneBox.style.transform = 'scale(1)'
+});
